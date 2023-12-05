d1683a3b158f54433ea83242427168f6e5103b6f
Ajaykum01 <115863768+Ajaykum01@users.noreply.github.com>
2023-12-05 20:38:43 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -41,7 +41,7 @@ async def save_group(bot, message):
                     InlineKeyboardButton('Help ⚡', url=f"https://t.me/{temp.U_NAME}?start=help"),
                     InlineKeyboardButton('My Channel 👀', url='https://t.me/+jARGKkERm1RlNDU1')
                  ],[
-                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Nexus_Shubhu")
+                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/ModAppsforyou1")
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -64,7 +64,7 @@ async def save_group(bot, message):
                                                                            InlineKeyboardButton('Help ⚡', url=f"https://t.me/{temp.U_NAME}?start=help"),
                                                                            InlineKeyboardButton('My Channel 👀', url='https://t.me/+jARGKkERm1RlNDU1')
                                                                         ],[
-                                                                           InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Nexus_Shubhu")
+                                                                           InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/ModAppsforyou1")
                                                                          ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
